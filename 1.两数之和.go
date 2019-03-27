7cb4fa2d2eb9132ea4a74e72e1982f4f8c25fd94
zhaonn <zhaonn@knowbox.cn>
2019-03-27 12:56:21 +0800

    实现两数之和

---
+++ 1.两数之和.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main()  {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+
+	fmt.Println(result)
+}
+
+func twoSum(num []int, target int)[]int {
+	numLength := len(num)
+	numMap := make(map[int]int, numLength)
+	retNum := make([]int, 2)
+
+	for i := 0; i < numLength; i++ {
+		lackValue := target - num[i]
+		if lackIndex, ok := numMap[num[i]]; ok {
+			retNum[0] = lackIndex
+			retNum[1] = i
+			return retNum
+		}
+
+		numMap[lackValue] = i
+	}
+
+	return retNum
+}
