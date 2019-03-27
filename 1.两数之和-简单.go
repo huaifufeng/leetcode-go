ec7bf097290b972ea3b57f5bee9bd71865307c20
zhaonn <zhaonn@knowbox.cn>
2019-03-27 13:55:03 +0800

    实现两数相加 中等

--- 1.两数之和-简单.go
+++ 1.两数之和-简单.go
@@ -1,10 +1,11 @@
 package main
 
 import "fmt"
-
+//题目地址：https://leetcode-cn.com/problems/two-sum/
 func main()  {
 	nums := []int{2, 7, 11, 15}
 	target := 9
+
 	result := twoSum(nums, target)
 
 	fmt.Println(result)
---
+++ 2.两数之和-中等.go
@@ -0,0 +1,58 @@
+package main
+//题目地址：https://leetcode-cn.com/problems/add-two-numbers/
+import "fmt"
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func (l *ListNode) print() {
+	currentNode := l
+	for currentNode != nil {
+		fmt.Println(currentNode.Val)
+		currentNode = currentNode.Next
+	}
+}
+
+func main() {
+	l1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	l2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+
+	result := addTwoNumbers(l1, l2)
+
+	result.print()
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var l3 *ListNode
+	l3Last := l3
+	upValue := 0  //相加后上一位的值
+	for l1 != nil || l2 != nil || upValue != 0 {
+		if l1 != nil {
+			upValue += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			upValue += l2.Val
+			l2 = l2.Next
+		}
+
+		currentVal := upValue % 10
+		upValue = upValue / 10
+
+		currentNode := &ListNode{currentVal, nil}
+		if l3 == nil {
+			l3 = currentNode
+			l3Last = currentNode
+			continue
+		}
+
+		l3Last.Next = currentNode
+		l3Last = currentNode
+	}
+
+	return l3
+}
+
