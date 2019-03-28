1f1b8fa1ac470c7cda5e18492030cd9441afd94c
zhaonn <zhaonn@knowbox.cn>
2019-03-28 12:51:49 +0800

    只出现一次的数字

---
+++ 初级算法/数组/5.只出现一次的数字.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{4,1,2,1,2}
+
+	result := singleNumber(nums)
+
+	fmt.Println(result)
+}
+
+func singleNumber(nums []int) int {
+	hasMap := make(map[int]int, len(nums))
+	for _,val := range nums {
+		if _,ok := hasMap[val];ok {
+			delete(hasMap,val)
+		} else {
+			hasMap[val] = 1
+		}
+	}
+
+	key := 0
+	for key = range hasMap {
+		break
+	}
+
+	return key
+}
