bdc80ff7b871d368ee0322048bc781e6035fa255
huaifufeng <huaifufeng@163.com>
2019-12-18 23:21:14 +0800

    完善代码

--- tests/questionBankTest/7.reverse-integer_test.go
+++ tests/questionBankTest/7.reverse-integer_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestReverseInteger(t *testing.T)  {
 	reverseInt := questionBank.Reverse7(1534236469)
-	if reverseInt != 10 {
+	if reverseInt != 0 {
 		t.Log(reverseInt)
 		t.Error("正数反转失败")
 	}
