b63a14a5a89f4d4729bcfd4dffe157b16b521f5b
huaifufeng <huaifufeng@163.com>
2020-03-03 22:15:55 +0800

    合并有序数组

---
+++ interviewQuestion/10.01.sorted-merge-lcci.go
@@ -0,0 +1,34 @@
+/**
+  题目地址：https://leetcode-cn.com/problems/sorted-merge-lcci/
+ */
+package interviewQuestion
+
+func merge(A []int, m int, B []int, n int)  {
+	pos := m + n - 1
+
+	for m > 0 || n > 0{
+		if m > 0 && n > 0 {
+			if A[m - 1] >= B[n - 1]{
+				A[pos] = A[m - 1]
+				m--
+			} else {
+				A[pos] = B[n-1]
+				n--
+			}
+		} else if m > 0 && n == 0 {
+			A[pos] = A[m - 1]
+			m--
+		} else {
+			A[pos] = B[n-1]
+			n--
+		}
+
+		pos--
+	}
+}
+
+func Merge(A []int, m int, B []int, n int) []int {
+	merge(A, m, B, n)
+
+	return A
+}
---
+++ tests/interviewQuestionTest/10.01.sorted-merge-lcci_test.go
@@ -0,0 +1,19 @@
+package interviewQuestionTest
+
+import (
+	"leetcode-go/interviewQuestion"
+	"testing"
+)
+
+func TestSortedMergeLcci(t *testing.T)  {
+	A := []int{1,2,3,0,0,0}
+	m := 3
+	B := []int{2,5,6}
+	n := 3
+
+	ret := interviewQuestion.Merge(A, m, B, n)
+
+	if ret[5] != 6 {
+		t.Error("结果不对")
+	}
+}
