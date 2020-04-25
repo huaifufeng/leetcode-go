63a2ccc20ac78b4cac1d107f19e1d8f6ca302351
huaifufeng <huaifufeng@foxmail.com>
2020-04-25 20:56:10 +0800

    完善

--- algorithm/radixSort_test.go
+++ algorithm/radixSort_test.go
@@ -1,15 +1,13 @@
 package algorithm
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestRadixSort(t *testing.T) {
-	nums := []string{"abc", "ad", "cdb"}
+	nums := []string{"bda", "cfd", "qwe", "yui", "abc", "rrr", "uue"}
 	RadixSort(nums)
 
-	fmt.Println(nums)
 	for i := 0; i < len(nums)-1; i++ {
 		if nums[i] > nums[i+1] {
 			t.Error("选择排序结果有问题")
