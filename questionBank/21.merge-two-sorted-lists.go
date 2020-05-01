0324f119fde802f6aab7c3bd6da2227c73684a42
huaifufeng <huaifufeng@foxmail.com>
2020-05-01 14:41:08 +0800

    perf(mergeSortedList): 增加空判断

--- questionBank/21.merge-two-sorted-lists.go
+++ questionBank/21.merge-two-sorted-lists.go
@@ -45,6 +45,13 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 }
 
 func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
 	l := new(ListNode)
 	first := l
 
