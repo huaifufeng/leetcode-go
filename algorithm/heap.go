7a40cd98dca365108b82803d377cec0780b94fd8
huaifufeng <huaifufeng@foxmail.com>
2020-05-07 08:18:25 +0800

    feat(heap): 实现堆的插入和删除

---
+++ algorithm/heap.go
@@ -0,0 +1,65 @@
+package algorithm
+
+type Heap struct {
+	data   []int
+	length int
+	count  int
+}
+
+//这里实现的是大顶堆
+func NewHeap(len int) (heap *Heap) {
+	heap = &Heap{
+		data:   make([]int, len+1),
+		length: len,
+		count:  0,
+	}
+
+	return
+}
+
+//从下向上堆化
+func (h *Heap) insert(val int) {
+	if h.length == h.count {
+		return
+	}
+
+	//数组从1开始， 0不存储数据
+	i := h.count + 1
+	h.data[i] = val
+	h.count++
+	//当在堆内，并且子节点比父节点打 进行交换处理
+	for i/2 > 0 && h.data[i] > h.data[i/2] {
+		h.data[i], h.data[i/2] = h.data[i/2], h.data[i]
+		i = i / 2
+	}
+}
+
+//从上向下堆化
+func (h *Heap) delete() {
+	if h.count == 0 {
+		return
+	}
+
+	h.data[1] = h.data[h.count]
+	h.count--
+
+	index := 1
+	maxPos := 1
+	for {
+		if 2*index <= h.count && h.data[2*index] > h.data[index] {
+			maxPos = 2 * index
+		}
+
+		if 2*index+1 <= h.count && h.data[2*index+1] > h.data[maxPos] {
+			maxPos = 2*index + 1
+		}
+
+		if maxPos == index {
+			break
+		}
+
+		h.data[index], h.data[maxPos] = h.data[maxPos], h.data[index]
+
+		index = maxPos
+	}
+}
---
+++ algorithm/heap_test.go
@@ -0,0 +1,29 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHeapInsert(t *testing.T) {
+	heap := NewHeap(5)
+	heap.insert(1)
+	heap.insert(2)
+	heap.insert(3)
+	heap.insert(4)
+	heap.insert(10)
+	heap.insert(14)
+
+	fmt.Println(heap)
+
+	heap.delete()
+	heap.delete()
+	heap.delete()
+	heap.delete()
+
+	heap.insert(2)
+	heap.insert(21)
+
+	fmt.Println(heap)
+
+}
