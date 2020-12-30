93c923cc2d2ec35273494a6176e80848aa558734
huaifufeng <huaifufeng@163.com>
2020-12-30 22:18:51 +0800

    整理代码

--- 初级算法/字符串/1.反转字符串.go
+++
@@ -1,9 +0,0 @@
-//题目地址：https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/32/
-package 字符串
-
-func reverseString(s []byte) {
-	length := len(s)
-	for i := 0; i < length/2; i++ {
-		s[i], s[length-1-i] = s[length-1-i], s[i]
-	}
-}
